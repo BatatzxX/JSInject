8f960aa4680950497834d8084a810a88551bdf32
BatatzxX <117692543+BatatzxX@users.noreply.github.com>
2022-11-09 21:56:07 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+/* this is a bookmarklet */
+javascript:const jssource = prompt('JSInject \nBy: BatatizxX \n \nInsert your code here:'); 
+document.write('<script>' + jssource+ '</script>');
+/* copy this 2 lines and add in a new browser bookmarklet */

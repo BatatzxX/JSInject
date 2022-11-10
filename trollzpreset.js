0af67f2a8248c8f15712e9c61bdc9937471ffcba
BatatzxX <117692543+BatatzxX@users.noreply.github.com>
2022-11-09 22:11:56 -0300

    Edit trollzpreset

--- trollzpreset.js
+++ trollzpreset.js
@@ -1,7 +1,7 @@
 const jssource = prompt('JSInject \nBy: BatatizxX \n \nType anything to start trollzpreset...:'); 
 document.write('<script>document.write("RIP BOZO");
-alert("your Pc is a potato now! XDDDDD")
+alert("your Pc is a potato now! XDDDDD");
 document.write("potatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotato");
-document.write("<h1>PO TA TOOOOOOOOOOOOO</h1>
-alert("potatooooooooo")</script>');
-/* Refresh the page to the troll end */
+document.write("<h1>PO TA TOOOOOOOOOOOOO</h1>;
+alert("potatooooooooo");</script>');
+/* Refresh the page to the troll end XDDDDDDD */
